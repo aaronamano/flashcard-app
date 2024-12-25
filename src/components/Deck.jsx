50b81b7142a26581d37f188afcd612a149c8b256
aaronamano <aaroncedric2005@yahoo.com>
2024-12-25 01:25:03 -0500

    implemented a feature where i can hide the cards in a certain collection and show them again

--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FlashcardList from './FlashcardList';
 import NewFlashcardForm from './NewFlashcardForm';
 
 function Deck({ deck, onAddCard, onDeleteDeck, onDeleteCard }) {
+
+  const [ isVisible, setIsVisible ] = useState(true);
+
   const handleAddCard = (newCard) => {
     onAddCard(deck.id, newCard);
   };
@@ -15,14 +18,25 @@ function Deck({ deck, onAddCard, onDeleteDeck, onDeleteCard }) {
     onDeleteCard(deck.id, cardId);
   };
 
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
+
   return (
     <div className="deck">
       <div className="deck-header">
         <h2>{deck.name}</h2>
+        <button onClick={toggleVisibility}>
+          {isVisible ? 'Hide Cards' : 'Show Cards'}
+        </button>
         <button onClick={handleDeleteDeck} className="delete-deck-btn">Delete Deck</button>
       </div>
-      <NewFlashcardForm onAddFlashcard={handleAddCard} />
-      <FlashcardList flashcards={deck.cards} onDeleteCard={handleDeleteCard} />
+      {isVisible && (
+        <>
+          <NewFlashcardForm onAddFlashcard={handleAddCard} />
+          <FlashcardList flashcards={deck.cards} onDeleteCard={handleDeleteCard} />
+        </>
+      )}
     </div>
   );
 }
